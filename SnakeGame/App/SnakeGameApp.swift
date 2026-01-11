1641201ead49576bd538b9029bd6a9efa39fb983
Dikaiosyne <JAM2199562@users.noreply.github.com>
2026-01-12 02:02:05 +0800

    auto: claude changes

--- SnakeGame/App/SnakeGameApp.swift
+++ SnakeGame/App/SnakeGameApp.swift
@@ -2,9 +2,12 @@ import SwiftUI
 
 @main
 struct SnakeGameApp: App {
+    @StateObject private var themeManager = ThemeManager()
+
     var body: some Scene {
         WindowGroup {
             GameView()
+                .environmentObject(themeManager)
         }
     }
 }
